9bce0c5e297cf285f63cd466bbbfbee393f0db10
Ivan Frias <friasgilivan@gmail.com>
2023-01-26 13:07:28 +0100

    Añadimos una persona para probar como subir por ramas

--- src/main/java/org/example/poo/main2.java
+++ src/main/java/org/example/poo/main2.java
@@ -11,6 +11,7 @@ public class main2 {
         Persona p1 = new Persona("Ivan", 25);
         Persona p2 = new Persona("Marta", 21);
         Persona p3 = new Persona("SomeOne", 32);
+        Persona p4 = new Persona("Misco", 32);
 
         showMenu(); // Estamos usando un método estatico de la clase UIMenu sin tener que poner el nombre de la clase
         // delante. to es gracias a la importacion que hemos hecho.
@@ -19,6 +20,7 @@ public class main2 {
         System.out.println("Para " + p1.getName() + " tenemos el id: " + p1.getId());
         System.out.println("Para " + p2.getName() + " tenemos el id: " + p2.getId());
         System.out.println("Para " + p3.getName() + " tenemos el id: " + p3.getId());
+        System.out.println("Para " + p4.getName() + " tenemos el id: " + p4.getId());
         System.out.println("Imprimimos la Constante de Faraday: " + FARADAY);
     }
 }
