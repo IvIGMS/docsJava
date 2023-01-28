045c433d3f4f64cd3bc00e28718f65160d6f8305
Ivan Frias <friasgilivan@gmail.com>
2023-01-28 15:58:47 +0100

    Modificacion de algunas clases de la parte de POO

--- src/main/java/org/example/poo/Casa.java
+++ src/main/java/org/example/poo/Casa.java
@@ -1,11 +1,30 @@
 package org.example.poo;
 
+/*
+* Creamos una clase que sirve para ver que podemos instanciar una clase dentro de otra.
+* */
 public class Casa {
     private String direcction;
     private int number;
 
     public Casa(String direcction, int number) {
         this.direcction = direcction;
+        this.number = number;
+    }
+
+    public String getDirecction() {
+        return direcction;
+    }
+
+    public void setDirecction(String direcction) {
+        this.direcction = direcction;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
         this.number = number;
     }
 
